21a339ea3a262344dd6839864de85e4dac7f346a
Ab-ol-fa-zl <113337879+Ab-ol-fa-zl@users.noreply.github.com>
2023-05-11 10:58:27 +0200

    hhh

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "Abolfazl";
+$password = ";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
